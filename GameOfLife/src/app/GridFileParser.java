00a111140b077838875f5e638c963787571ad15d
Alexandru <alexplacinta94@gmail.com>
2016-04-04 02:08:07 +0300

    sa

--- GameOfLife/src/app/GridFileParser.java
+++ GameOfLife/src/app/GridFileParser.java
@@ -16,27 +16,25 @@ public class GridFileParser {
 	}
 	
 	public byte[][] create2DIntGridFromFile(String fileLocation) throws IOException {
-	    BufferedReader buffer = new BufferedReader(new FileReader(fileLocation));
-	    
-	    byte[][] grid = null;
-	    String line;
-	    List<String> list = new ArrayList<String>();
-
-        while ((line = buffer.readLine()) != null) {
-            list.add(line);
-        }
-        
-        grid = new byte[list.size()][list.get(0).length()];
-        int i = 0;
-        for (String l : list) {
-        	for (int j = 0; j < l.length(); j++) {
-        		grid[i][j] = (byte) (l.charAt(j) - 48);
-//        		System.out.print(grid[i][j]);
-        	}
-//        	System.out.println();
-        	i++;
-        }
-        
-	    return grid;
+		BufferedReader buffer = new BufferedReader(new FileReader(fileLocation));
+		
+		byte[][] grid = null;
+		String line;
+		List<String> list = new ArrayList<String>();
+		
+		while ((line = buffer.readLine()) != null) {
+		    list.add(line);
+		}
+		
+		grid = new byte[list.size()][list.get(0).length()];
+		int i = 0;
+		for (String l : list) {
+			for (int j = 0; j < l.length(); j++) {
+				grid[i][j] = (byte) (l.charAt(j) - 48);
+			}
+			i++;
+		}
+		
+		return grid;
 	}
 }
