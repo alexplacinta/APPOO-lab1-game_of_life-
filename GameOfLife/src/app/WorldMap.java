fe890e17dc49470a42ab0019cf37b477fe90c484
Alexandru <alexplacinta94@gmail.com>
2016-04-04 04:29:22 +0300

    some refactoring in term of buffer for grid processing

--- GameOfLife/src/app/WorldMap.java
+++ GameOfLife/src/app/WorldMap.java
@@ -1,48 +1,44 @@
 package app;
 
 public class WorldMap {
-	
-	private TwoSizeBufferCellGrid buffer;
+	/** actual grid */
+	private CellGrid currentGrid;
+	/** buffer grid that help to perform processing */
+	private CellGrid bufferGrid;
 
 	public WorldMap(byte[][] inputGrid) {
-		
-		buffer = new TwoSizeBufferCellGrid(
-				new CellGrid(inputGrid), 
-				new CellGrid(inputGrid));
+		currentGrid = new CellGrid(inputGrid);
+		bufferGrid = new CellGrid(inputGrid);
 	}
 
 	public void evolve() {
-		CellGrid currentGrid = buffer.getCurrentCellGrid();
-		CellGrid anotherGrid = buffer.getAnotherCellGrid();
 		
 		for (int i = 0; i < currentGrid.getYLength(); i++) {
 			for (int j = 0; j < currentGrid.getXLength(); j++) {
 				int aliveNeighbours = currentGrid.getNumAliveNeighbours(i, j);
 				
 				if ((aliveNeighbours < 2) || (aliveNeighbours > 3)) {
-					anotherGrid.setState(i, j, (byte)0);
+					bufferGrid.setState(i, j, (byte)0);
 				} else if (aliveNeighbours == 3) {
-					anotherGrid.setState(i, j, (byte)1);
+					bufferGrid.setState(i, j, (byte)1);
 				} else {
-					anotherGrid.setState(i, j, currentGrid.getState(i, j));
+					bufferGrid.setState(i, j, currentGrid.getState(i, j));
 				}
 			}
 		}
 		
-		buffer.switchPointer();
-		
+		switchCurrentGridWithBuffer();
 	}
 	
-	// this will point to current grid of cells from buffer
-	private CellGrid getCurrentGrid() {
-		return buffer.getCurrentCellGrid();
+	private void switchCurrentGridWithBuffer() {
+		CellGrid tempGrid = bufferGrid;
+		bufferGrid = currentGrid;
+		currentGrid = tempGrid;
 	}
 	
 	@Override
 	public String toString() {
-		CellGrid grid = getCurrentGrid();
-		
-		return grid.toString();
+		return currentGrid.toString();
 	}
 
 }
